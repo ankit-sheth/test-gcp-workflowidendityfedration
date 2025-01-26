e988249e5b0b28ae0eccd68132ef2495ed9d9ff9
Ankit <ankitsheth111@gmail.com>
2025-01-26 11:16:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,3 @@ resource "google_storage_bucket" "static" {
  uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket" "static" {
- name          = "test-oidc-bucket-2"
- location      = "US"
- storage_class = "STANDARD"
- uniform_bucket_level_access = true
-}
-
