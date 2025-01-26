c8fae885502e747f85b1e5c2221336389603ac41
Ankit <ankitsheth111@gmail.com>
2025-01-26 11:13:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "test-oidc-bucket"
+  name          = "test-bucket-oidc"
+  location      = "us-central1"
+  project = "pse-csim"
+  force_destroy = true
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket" "my-bucket2" {
+  name          = "test-bucket-oidc-002"
   location      = "us-central1"
   project = "pse-csim"
   force_destroy = true
